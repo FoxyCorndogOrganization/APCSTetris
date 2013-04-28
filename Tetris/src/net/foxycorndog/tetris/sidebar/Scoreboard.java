4f39676ab282eccea11a8c47ef3e1e568c90eb32
Braden Steffaniak <BradenSteffaniak@gmail.com>
2013-04-27 19:54:44 -0500

    Added Henry as the Author\!

--- Tetris/src/net/foxycorndog/tetris/sidebar/Scoreboard.java
+++ Tetris/src/net/foxycorndog/tetris/sidebar/Scoreboard.java
@@ -3,17 +3,18 @@ package net.foxycorndog.tetris.sidebar;
 /**
  * 
  * 
+ * @author	Henry Rybolt
  * @author	Braden Steffaniak
  * @since	Apr 23, 2013 at 4:54:12 PM
  * @since	v0.1
- * @version	Apr 23, 2013 at 4:54:12 PM
+ * @version	Apr 27, 2013 at 12:43:29 PM
  * @version	v0.1
  */
-public class ScoreBoard
+public class Scoreboard
 {
 	int score;
 	
-	public ScoreBoard()
+	public Scoreboard()
 	{
 		score = 0;
 	}
--- Tetris/src/net/foxycorndog/tetris/sidebar/Sidebar.java
+++ Tetris/src/net/foxycorndog/tetris/sidebar/Sidebar.java
@@ -3,20 +3,21 @@ package net.foxycorndog.tetris.sidebar;
 /**
  * 
  * 
+ * @author	Henry Rybolt
  * @author	Braden Steffaniak
  * @since	Apr 23, 2013 at 4:43:29 PM
  * @since	v0.1
- * @version	Apr 23, 2013 at 4:43:29 PM
+ * @version	Apr 27, 2013 at 12:43:29 PM
  * @version	v0.1
  */
 public class Sidebar
 {
 	NextPiece nP;
-	ScoreBoard sB;
+	Scoreboard sB;
 	public Sidebar()
 	{
 		nP = new NextPiece();
-		sB = new ScoreBoard();
+		sB = new Scoreboard();
 	}
 	
 	public void render()
